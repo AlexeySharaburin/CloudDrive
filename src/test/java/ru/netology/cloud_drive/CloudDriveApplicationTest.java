69378ca429d216428eff4e11a430b858b78517c2
Alexey Sharaburin <alexey.sharaburin@gmail.com>
2021-06-17 16:32:10 +0200

    Correct Testcontainers

--- src/test/java/ru/netology/cloud_drive/CloudDriveApplicationTest.java
+++ src/test/java/ru/netology/cloud_drive/CloudDriveApplicationTest.java
@@ -4,21 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Map;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@ContextConfiguration(initializers = {CloudDriveApplicationTest.Initializer.class})
 @Testcontainers
 public class CloudDriveApplicationTest {
 
@@ -31,43 +27,25 @@ public class CloudDriveApplicationTest {
 
     @Container
     public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres")
+            .withNetwork(cloudNetwork)
+            .withNetworkAliases("postgresDB")
             .withDatabaseName("cloud_drive")
             .withUsername("alexey")
-            .withPassword("123")
-            .withNetwork(cloudNetwork)
-            .withNetworkAliases("postgresContainer");
+            .withPassword("123");
 
     @Container
     public static GenericContainer<?> backContainer = new GenericContainer<>("cloud_drive:5.0")
             .withNetwork(cloudNetwork)
-            .withNetworkAliases("backContainer")
             .withExposedPorts(8080)
+            .withEnv(Map.of("jdbc:postgresql://localhost:5432/cloud_drive", "jdbc:postgresql://postgresDB:5432/cloud_drive"))
             .dependsOn(postgresContainer);
-
-    @Container
-    public static GenericContainer<?> frontContainer = new GenericContainer<>("cloud_front")
-            .withNetwork(cloudNetwork)
-            .withNetworkAliases("front_container")
-            .withExposedPorts(8081)
-            .dependsOn(backContainer);
-//
-//
-//    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-//        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-//            TestPropertyValues.of(
-//                    "spring.datasource.url=" + postgresContainer.getJdbcUrl(),
-//                    "spring.datasource.username=" + postgresContainer.getUsername(),
-//                    "spring.datasource.password=" + postgresContainer.getPassword()
-//            ).applyTo(configurableApplicationContext.getEnvironment());
-//        }
-//    }
 
 
     @Test
     void contextPostgres() {
         var portDatabase = postgresContainer.getMappedPort(5432);
         System.out.println(postgresContainer.getJdbcUrl() + " " + postgresContainer.getDatabaseName() + " " + postgresContainer.getPassword());
-        System.out.println(cloudNetwork.getId());
+        System.out.println("Network ID: " + cloudNetwork.getId());
         System.out.println("Port CloudDrive Database: " + portDatabase);
         Assertions.assertTrue(postgresContainer.isRunning());
     }
@@ -81,22 +59,91 @@ public class CloudDriveApplicationTest {
         Assertions.assertTrue(backContainer.isRunning());
     }
 
-    @Test
-    void contextFront() {
-        var portFront = frontContainer.getMappedPort(8081);
-        ResponseEntity<String> forEntityFront = restTemplate.getForEntity(HOST + portFront, String.class);
-        System.out.println("Port CloudDrive Frontend: " + portFront);
-        System.out.println(forEntityFront.getBody());
-        Assertions.assertTrue(frontContainer.isRunning());
-    }
-
 }
 
 
 
 
 
-
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+////@ContextConfiguration(initializers = {CloudDriveApplicationTest.Initializer.class})
+//@Testcontainers
+//public class CloudDriveApplicationTest {
+//
+//    private final String HOST = "http://localhost:";
+//
+//    @Autowired
+//    TestRestTemplate restTemplate;
+//
+//    static Network cloudNetwork = Network.newNetwork();
+//
+//    @Container
+//    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres")
+//            .withNetwork(cloudNetwork)
+//            .withNetworkAliases("postgresDB")
+//            .withDatabaseName("cloud_drive")
+//            .withUsername("alexey")
+//            .withPassword("123")
+////            .withExposedPorts(5432)
+//            ;
+////
+//
+//    @Container
+//    public static GenericContainer<?> backContainer = new GenericContainer<>("cloud_drive:5.0")
+//            .withNetwork(cloudNetwork)
+////            .withNetworkAliases("backContainer")
+//            .withExposedPorts(8080)
+//            .withEnv(Map.of("jdbc:postgresql://localhost:5432/cloud_drive", "jdbc:postgresql://postgresDB:5432/cloud_drive"))
+//            .dependsOn(postgresContainer);
+//
+////    @Container
+////    public static GenericContainer<?> frontContainer = new GenericContainer<>("cloud_front")
+////            .withNetwork(cloudNetwork)
+////            .withNetworkAliases("front_container")
+////            .withExposedPorts(8080);
+////            .dependsOn(backContainer);
+////
+////
+////    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+////        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+////            TestPropertyValues.of(
+////                    "spring.datasource.url=" + postgresContainer.getJdbcUrl(),
+////                    "spring.datasource.username=" + postgresContainer.getUsername(),
+////                    "spring.datasource.password=" + postgresContainer.getPassword()
+////            ).applyTo(configurableApplicationContext.getEnvironment());
+////        }
+////    }
+//
+//
+//    @Test
+//    void contextPostgres() {
+////        var portDatabase = postgresContainer.getMappedPort(5432);
+////        System.out.println(postgresContainer.getJdbcUrl() + " " + postgresContainer.getDatabaseName() + " " + postgresContainer.getPassword());
+////        System.out.println("Network ID: " + cloudNetwork.getId());
+////        System.out.println("Port CloudDrive Database: " + portDatabase);
+//        Assertions.assertTrue(postgresContainer.isRunning());
+//    }
+//
+//    @Test
+//    void contextBack() {
+//        var portBack = backContainer.getMappedPort(8080);
+//        ResponseEntity<String> forEntityBackApp = restTemplate.getForEntity(HOST + portBack, String.class);
+//        System.out.println("\nPort Cloud Drive backend: " + portBack);
+//        System.out.println(forEntityBackApp.getBody());
+//        Assertions.assertTrue(backContainer.isRunning());
+//    }
+//
+////    @Test
+////    void contextFront() {
+////        var portFront = frontContainer.getMappedPort(8080);
+////        ResponseEntity<String> forEntityFront = restTemplate.getForEntity(HOST + portFront, String.class);
+////        System.out.println("Port CloudDrive Frontend: " + portFront);
+////        System.out.println("Network ID: " + cloudNetwork.getId());
+////        System.out.println(forEntityFront.getBody());
+////        Assertions.assertTrue(frontContainer.isRunning());
+////    }
+//
+//}
 
 
 //    @Container
